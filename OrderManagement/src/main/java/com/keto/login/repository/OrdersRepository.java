fe75eb4e2eef56fa46544abe52acdd95df7d9aea
Sujay <sujaydsa@traform.com>
2020-10-17 16:03:31 +0530

    made the results of findall cacheable.

--- OrderManagement/src/main/java/com/keto/login/repository/OrdersRepository.java
+++ OrderManagement/src/main/java/com/keto/login/repository/OrdersRepository.java
@@ -1,5 +1,8 @@
 package com.keto.login.repository;
 
+import java.util.List;
+
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -8,5 +11,7 @@ import com.keto.login.model.Orders;
 @Repository
 public interface OrdersRepository extends JpaRepository<Orders, Long> {
    
-
+	@Override
+	@Cacheable("ordersCache")
+	List<Orders> findAll();
 }
